0d8e9db1bbe1d6933f513d4c2088654f992b366a
kr-chirag <chirag.p@knackroot.com>
2025-03-12 12:03:50 +0530

    added authentication

---
+++ src/services/user.service.ts
@@ -0,0 +1,23 @@
+import User from "../models/user.model";
+
+export async function createUser(name: string, email: string, pass: string) {
+    const result = await User.create({ name, email, password: pass });
+    return {
+        id: result.id,
+        name: result.name,
+        email: result.email,
+        password: result.password,
+    };
+}
+
+export async function getUserByEmail(email: string) {
+    return await User.findOne({ email });
+}
+
+export async function getUserByID(userId: string) {
+    return await User.findById(userId).select("-password");
+}
+
+export async function getAllUsers() {
+    return await User.find({}).select("-password");
+}
