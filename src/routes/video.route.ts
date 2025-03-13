58206fb8a569141421c9792b301c3579f80f2cb6
kr-chirag <chirag.p@knackroot.com>
2025-03-13 09:10:37 +0530

    fix: auth middleware removed from video router and added to specific video routes only

--- src/routes/video.route.ts
+++ src/routes/video.route.ts
@@ -11,13 +11,14 @@ import { checkAuth } from "../middlewares/auth.middleware";
 
 const videoRouter = express.Router();
 
-videoRouter.use(checkAuth);
-
-videoRouter.post("/", uploadSingleFiles("video"), handlePostVideo);
-videoRouter.route("/:videoId").get(handleGetVideo).delete(handleDeleteVideo);
+videoRouter.post("/", checkAuth, uploadSingleFiles("video"), handlePostVideo);
+videoRouter
+    .route("/:videoId")
+    .get(handleGetVideo)
+    .delete(checkAuth, handleDeleteVideo);
 videoRouter
     .route("/:videoId/like")
-    .post(handlePostLike)
-    .delete(handleDeleteLike);
+    .post(checkAuth, handlePostLike)
+    .delete(checkAuth, handleDeleteLike);
 
 export default videoRouter;
